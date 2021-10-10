68823ac5caa942f0773d191f6ff3f271ac91a9ac
Bo Svensson <90132211+bosvensson1@users.noreply.github.com>
2021-10-10 08:05:06 +0000

    Update build_googletest.sh

--- CI/build_googletest.sh
+++ CI/build_googletest.sh
@@ -5,10 +5,6 @@ cd googletest
 mkdir build
 cd build
 cmake \
-    -D CMAKE_C_COMPILER="${CC}" \
-    -D CMAKE_CXX_COMPILER="${CXX}" \
-    -D CMAKE_C_COMPILER_LAUNCHER=ccache \
-    -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -D CMAKE_BUILD_TYPE="Release" \
     -D CMAKE_INSTALL_PREFIX="$(pwd)/googletest/build/install" \
     -G "Unix Makefiles" \
